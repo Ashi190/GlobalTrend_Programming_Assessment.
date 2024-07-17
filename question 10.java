bf4b0c0526a5b5b074de137d62cb256067e47aa7
ashi sharma <ashisharma1920@gmail.com>
2024-07-18 00:29:42 +0530

    Add files via upload

---
+++ question 10.java
@@ -0,0 +1,32 @@
+package GlobalTrendProgrammingAssessment;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String str) {
+        // Convert to lowercase and remove non-alphanumeric characters
+        String normalizedStr = str.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        // Check if the string is a palindrome
+        int left = 0;
+        int right = normalizedStr.length() - 1;
+
+        while (left < right) {
+            if (normalizedStr.charAt(left) != normalizedStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "A man, a plan, a canal, Panama";
+        System.out.println(str1 + " is a palindrome? " + isPalindrome(str1)); // Output: true
+
+        String str2 = "race a car";
+        System.out.println(str2 + " is a palindrome? " + isPalindrome(str2)); // Output: false
+    }
+}
+
